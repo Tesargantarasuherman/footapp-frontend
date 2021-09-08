8c9dbf5343cc315c5dbb5af2b6ac8cc74db91594
Iamsuherman <tgantara@gmail.com>
2021-09-08 13:48:37 +0700

    update

--- src/pages/Artikel.js
+++ src/pages/Artikel.js
@@ -1,7 +1,53 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
 
 class Artikel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            offset: 0,
+            data: [],
+            perPage: 1,
+            currentPage: 0,
+        };
+        this.handlePageClick = this.handlePageClick.bind(this);
+    }
+    receivedData() {
+        axios.get(`http://localhost:8000/artikel`).then(res => {
+            const data = res.data.data;
+            console.log(data.length)
+            const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
+            const postData = slice.map(pd =>
+                <React.Fragment>
+                    <p>{pd.judul}</p>
+                    <p>{pd.deskripsi}</p>
+                </React.Fragment>)
+
+            this.setState({
+                pageCount: Math.ceil(data.length / this.state.perPage),
+
+                postData
+            })
+        });
+    }
+
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
+
+        this.setState({
+            currentPage: selectedPage,
+            offset: offset
+        }, () => {
+            this.receivedData()
+        });
+
+    };
+    componentDidMount() {
+        this.receivedData()
+    }
     render() {
         return (
             <div className="container body-utama-artikel">
@@ -57,12 +103,15 @@ class Artikel extends Component {
                     <div className="pagination d-flex justify-content-center">
                         <nav aria-label="">
                             <ul class="pagination">
+
                                 <li class="page-item">
                                     <a class="page-link" href="#" aria-label="Previous">
                                         <span aria-hidden="true">&laquo;</span>
                                         <span class="sr-only">Previous</span>
                                     </a>
                                 </li>
+
+
                                 <li class="page-item"><a class="page-link" href="#">1</a></li>
                                 <li class="page-item"><a class="page-link" href="#">2</a></li>
                                 <li class="page-item"><a class="page-link" href="#">3</a></li>
@@ -72,9 +121,27 @@ class Artikel extends Component {
                                         <span class="sr-only">Next</span>
                                     </a>
                                 </li>
+
                             </ul>
                         </nav>
+
                     </div>
+                    <div>
+                        {this.state.postData}
+                        <ReactPaginate
+                            previousLabel={"prev"}
+                            nextLabel={"next"}
+                            breakLabel={"..."}
+                            breakClassName={"break-me"}
+                            pageCount={this.state.pageCount}
+                            marginPagesDisplayed={2}
+                            pageRangeDisplayed={5}
+                            onPageChange={this.handlePageClick}
+                            containerClassName={"pagination"}
+                            subContainerClassName={"pages pagination"}
+                            activeClassName={"active"} />
+                    </div>
+
                 </section>
             </div>
         );
